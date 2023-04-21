9a3d776ff018437f331bb891beecdd40a0cb3bf3
262882 <2628829@students.wits.ac.za>
2023-04-21 21:05:32 +0200

    Process list of models

--- evaluate/eval_modular.py
+++ evaluate/eval_modular.py
@@ -18,9 +18,12 @@ from myloader import CocoDetection
 from mymodels import nanodet
 from mynms import multiclass_nms
 
-display = True
+display = False
 
-resFile = "../results/nano_stats0.json"
+evaluate_models = [
+    ['../models/finedet_map93_416.onnx', 416],
+    ['../models/nanodet-plus-m-1.5x_416.onnx', 416],
+]
 
 dataset_dirs = [
     "../data/coco_validation_sml_01_False_drill/",  # No match, no occl
@@ -28,96 +31,98 @@ dataset_dirs = [
     "../data/coco_validation_sml_01_FalseTrue_matchdrill/"  # Full
 ]
 
-model_name = 'nanodet0'
-model = nanodet('../models/finedet_map93_416.onnx', 416)
-#model = nanodet('../models/nanodet-plus-m-1.5x_416.onnx', 416)
-
-stats = {}
-x_loop_must_break = False
-for set_no, dir in enumerate(dataset_dirs):
-    set_name = dir[dir[:-1].rfind('/')+1:-1]
-    print('Evaluating dataset', set_no)
-    img_dir = os.path.abspath(dir)
-    annotation = img_dir + "/annot.json"
-    dataset = CocoDetection(root=img_dir, annFile=annotation)
-
-    preds = []
-    for idx, id in enumerate(dataset.ids):
-        img, target = dataset[idx] #35 27
-
-        # Prepare prediction 
-        detections = model.forward(img)
+for eval_model in evaluate_models:
         
-        if len(detections)>0:
-            for detect in detections:  # [x_min, y_min, x_max, y_max, score, class]
-                scores = detect[4].tolist()
-                
-                boxes = detect[0:4]
-                labels = detect[5]
-                
-                # Consider only ball detections
-                if labels == 32:
-                    labels = 37  # Convert prediction to coco
-                    pass
-                else:
-                    continue
-                
-                if (display):
-                    # Display the resulting frame
-                    print(detect)
-                    frame = cv2.rectangle(np.asarray(img), (int(detect[0]), int(detect[1])), (int(detect[2]), int(detect[3])),(0, 255, 0), 2)
-                    frame = cv2.circle(np.asarray(frame), (int(detect[-2]), int(detect[-1])),5,(255, 0, 0), 2)
-                    cv2.imshow('Frame',frame)
+    model_name = eval_model[0][eval_model[0].rfind('/')+1:-5]
+    resFile = "../results/" + model_name + "_stats.json"
+    model = nanodet(eval_model[0], eval_model[1])
 
-                    # Press Q on keyboard to  exit
-                    if cv2.waitKey(0) & 0xFF == ord('q'):
-                        x_loop_must_break = True
-                        break
+    stats = {}
+    x_loop_must_break = False
+    for set_no, dir in enumerate(dataset_dirs):
+        set_name = dir[dir[:-1].rfind('/')+1:-1]
+        print('Evaluating', model_name, "using dataset", set_no)
+        img_dir = os.path.abspath(dir)
+        annotation = img_dir + "/annot.json"
+        dataset = CocoDetection(root=img_dir, annFile=annotation)
 
-                # from xyxy to xywh
-                boxes[2] = boxes[2]-boxes[0]
-                boxes[3] = boxes[3]-boxes[1]
+        preds = []
+        for idx, id in enumerate(dataset.ids):
+            img, target = dataset[idx]
+
+            # Prepare prediction 
+            detections = model.forward(img)
+            
+            if len(detections)>0:
+                for detect in detections:  # [x_min, y_min, x_max, y_max, score, class]
+                    scores = detect[4].tolist()
                     
-                preds.append(
-                    dict(
-                        image_id = id,
-                        category_id=labels,
-                        bbox=boxes,
-                        score=round(scores,3),
+                    boxes = detect[0:4]
+                    labels = detect[5]
+                    
+                    # Consider only ball detections
+                    if labels == 32:
+                        labels = 37  # Convert prediction to coco
+                        pass
+                    else:
+                        continue
+                    
+                    if (display):
+                        # Display the resulting frame
+                        print(detect)
+                        frame = cv2.rectangle(np.asarray(img), (int(detect[0]), int(detect[1])), (int(detect[2]), int(detect[3])),(0, 255, 0), 2)
+                        frame = cv2.circle(np.asarray(frame), (int(detect[-2]), int(detect[-1])),5,(255, 0, 0), 2)
+                        cv2.imshow('Frame',frame)
+
+                        # Press Q on keyboard to  exit
+                        if cv2.waitKey(0) & 0xFF == ord('q'):
+                            x_loop_must_break = True
+                            break
+
+                    # from xyxy to xywh
+                    boxes[2] = boxes[2]-boxes[0]
+                    boxes[3] = boxes[3]-boxes[1]
+                        
+                    preds.append(
+                        dict(
+                            image_id = id,
+                            category_id=labels,
+                            bbox=boxes,
+                            score=round(scores,3),
+                        )
                     )
-                )
+            if x_loop_must_break:
+                break
+                
+        workingFile = "./detection_cocoresults_set.json"
+        with open(workingFile, 'w') as out_file:
+            json.dump(preds, out_file)
+
+        cocoGt = COCO(annotation)
+        cocoDt = cocoGt.loadRes(workingFile)
+        #os.remove(workingFile)
+
+        cocoEval = COCOeval(cocoGt=cocoGt, cocoDt=cocoDt, iouType='bbox')
+        cocoEval.params.imgIds  = dataset.ids  # Evaluate all images
+        cocoEval.params.useCats = True
+        cocoEval.params.catIds = 37  # Ball
+
+        cocoEval.evaluate()
+        cocoEval.accumulate()
+        print(set_name)
+        cocoEval.summarize()
+
+        stat_sum = {
+            'AP_{@[IoU=0.50:0.95]-all}': cocoEval.stats[0],
+            'AP_{@[IoU=0.50:0.95]-small}': cocoEval.stats[3],
+            'AP_{@[IoU=0.50:0.95]-medium}': cocoEval.stats[4],
+        }
+        stats[set_name] = stat_sum
+
         if x_loop_must_break:
             break
-            
-    workingFile = "./detection_cocoresults_set.json"
-    with open(workingFile, 'w') as out_file:
-        json.dump(preds, out_file)
 
-    cocoGt = COCO(annotation)
-    cocoDt = cocoGt.loadRes(workingFile)
-    #os.remove(workingFile)
+    detector_res = {model_name:stats}
 
-    cocoEval = COCOeval(cocoGt=cocoGt, cocoDt=cocoDt, iouType='bbox')
-    cocoEval.params.imgIds  = dataset.ids  # Evaluate all images
-    cocoEval.params.useCats = True
-    cocoEval.params.catIds = 37  # Ball
-
-    cocoEval.evaluate()
-    cocoEval.accumulate()
-    print(set_name)
-    cocoEval.summarize()
-
-    stat_sum = {
-        'AP_{@[IoU=0.50:0.95]-all}': cocoEval.stats[0],
-        'AP_{@[IoU=0.50:0.95]-small}': cocoEval.stats[3],
-        'AP_{@[IoU=0.50:0.95]-medium}': cocoEval.stats[4],
-    }
-    stats[set_name] = stat_sum
-
-    if x_loop_must_break:
-        break
-
-detector_res = {model_name:stats}
-
-with open(resFile, 'w') as out_file:
-    json.dump(detector_res, out_file)
+    with open(resFile, 'w') as out_file:
+        json.dump(detector_res, out_file)
