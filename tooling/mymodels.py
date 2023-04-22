67d416e0e6f4a4257f55d84d6af45f12495854d1
262882 <2628829@students.wits.ac.za>
2023-04-22 23:01:14 +0200

    Start detailing yolox

--- tooling/mymodels.py
+++ tooling/mymodels.py
@@ -156,18 +156,8 @@ class nanodet():
         return preds
 
 class yolox():
-    """`MS Coco Detection <http://mscoco.org/dataset/#detections-challenge2016>`_ Dataset.
 
-    Args:
-        root (string): Root directory where images are downloaded to.
-        annFile (string): Path to json annotation file.
-        transform (callable, optional): A function/transform that  takes in an PIL image
-            and returns a transformed version. E.g, ``transforms.ToTensor``
-        target_transform (callable, optional): A function/transform that takes in the
-            target and transforms it.
-    """
-
-    def __init__(self, dir, res):
+    def __init__(self, dir, res, num_classes=80):
         providers = ['CPUExecutionProvider']
         sess_options = rt.SessionOptions()  # https://onnxruntime.ai/docs/performance/tune-performance.html
         sess_options.intra_op_num_threads = 1
@@ -176,43 +166,98 @@ class yolox():
         self.session = rt.InferenceSession(dir, sess_options=sess_options, providers=providers)
         self.outname = [i.name for i in self.session.get_outputs()] 
         self.inname = [i.name for i in self.session.get_inputs()]
-        self.res = res
+        self.mod_res = res
+        self.num_classes = num_classes
+        self.strides = [8, 16, 32]
 
-    def _normalize(self, in_img): 
-        in_img = np.asarray(in_img).astype(np.float32) / 255
-        MEAN = np.array([0.406, 0.456, 0.485], dtype=np.float32)
-        STD = np.array([0.225, 0.224, 0.229], dtype=np.float32)
-        out_img = in_img - MEAN / STD
-        return out_img
+        self.grid_points = []
+        self.grid_strides = []
+        for stride in self.strides:
+            grid_point = self._make_grid_point(
+                (int(res/stride),
+                 int(res/stride)),
+                stride,
+            )
+            self.grid_points.extend(grid_point)
+            self.grid_strides.extend([stride]*len(grid_point))  
+
+    def _make_grid_point(self, grid_size, stride):
+        grid_height, grid_width = grid_size
+
+        shift_x = np.arange(0, grid_width) * stride
+        shift_y = np.arange(0, grid_height) * stride
+
+        xv, yv = np.meshgrid(shift_x, shift_y)
+        xv = xv.flatten()
+        yv = yv.flatten()
+
+        cx = xv #  + 0.5 * (stride - 1)
+        cy = yv #  + 0.5 * (stride - 1)
+
+        return np.stack((cx, cy), axis=-1)
+    
+    def _softmax(self, x, axis=1):
+        x_exp = np.exp(x)
+        x_sum = np.sum(x_exp, axis=axis, keepdims=True)
+        s = x_exp / x_sum
+        return s
+    
+    def _pre_process(self, img):
+        blob = cv2.dnn.blobFromImage(img, size = (self.mod_res, self.mod_res), # Resolution multiple of 32
+                                swapRB=False, crop=False) 
+        return blob
+
+
+    def _post_process(self, predict_results):
+        print("process")
+        preds = []
+        class_ind = 32
+        score_thr=0.05
+        PROJECT = np.arange(8)
+
+        class_scores = predict_results[:,6:]
+        bbox_predicts = predict_results[:,:4]
+        bbox_score = predict_results[:,5]
+
+        max_inds = np.argmax(bbox_score)  # Assume only one instance
+        print(max_inds)
+        if bbox_score[max_inds] > score_thr:
+            pred_inds = [[int(max_inds)]]
+        else:
+            pred_inds = []
+        #pred_inds = np.argwhere(class_scores[:, class_ind] > score_thr)  # Process multiple instances
+
+        print(pred_inds)
+        if len(pred_inds)>0:
+            for ind in pred_inds:
+                ind = ind[0]
+                x, y = self.grid_points[ind]
+                stride = self.grid_strides[ind]
+                p_x, p_y, p_w, p_h = bbox_predicts[ind,:4]
+                l_w, l_h = stride*np.exp(np.array([p_w, p_h]))
+                l_x, l_y = stride*np.array([p_x,p_y])
+
+                print(x, y, l_x, l_y, l_w, l_h,bbox_score[ind], np.argmax(class_scores[ind]))
+
+                preds.append([x-l_w//2, y-l_h//2, x+l_w//2, y+l_h//2, bbox_score[ind], np.argmax(class_scores[ind])])
+
+        return preds
 
     def forward(self, img):
-        detections = []
-        norm_img = self._normalize(img)
-        blob = cv2.dnn.blobFromImage(norm_img, 
-                        size = (self.res, self.res),
-                        swapRB=False, crop=False) 
-        inp = {self.inname[0]:blob}
-        layer_output = self.session.run(self.outname, inp)[0][0]          
+        print("forward")
+        preds = []
+        norm_img = np.asarray(img)
+        inp = {self.inname[0]:self._pre_process(norm_img)}
+        layer_output = self.session.run(self.outname, inp)[0][0]  
 
-        coco_ind = 32  # Ball
-        stride = 8  # Stride: 8,16,32
-        cell_count = self.res//stride
+        print(layer_output.shape)
 
-        #max_inds = np.argwhere(layer_output[:(cell_count)**2, coco_ind] > 0.2)
-        max_inds = np.argwhere(layer_output[:, coco_ind] > 0.5)
+        detections_pre = self._post_process(layer_output)   
+        for detection in detections_pre:
 
-        for ind in max_inds:
-            ind=ind[0]
-
-            # Find pixel representation
-            x = (ind%cell_count+0.5)*norm_img.shape[0]/cell_count
-            y = (ind//cell_count+0.5)*norm_img.shape[0]/cell_count
-
-            # Find max prediction as result
-            box = np.max(np.reshape(layer_output[ind,-32:],(4,8)),axis=1)
-            l,t,r,b = (box*norm_img.shape[0]/cell_count)
-            print("lrtb",l,r,t,b)
-
-            detections.append([x-l, y-t, x+r, y+b, layer_output[ind, coco_ind], coco_ind, x, y])
-
-        return detections
+            x1, y1, x2, y2 = np.array(detection[:4])*norm_img.shape[0]/self.mod_res
+            score = detection[-2]
+            ind = detection[-1]
+            preds.append([x1, y1, x2, y2, score, ind])
+            
+        return preds
